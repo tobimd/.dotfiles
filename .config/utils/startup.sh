bc374808fb5a5356acbbe821cc60873f309dc986
tobimd <33004093+tobimd@users.noreply.github.com>
2022-08-28 19:22:00 -0400

    add util scripts

---
+++ .config/utils/startup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/zsh
+
+if [[ $OSTYPE == "linux-gnu"* ]]; then
+    # Linux specific settings
+
+elif [[ $OSTYPE == "darwin"* ]]; then
+    # MacOS specific settings
+
+    PATH="$PATH:/usr/local/Homebrew/bin"
+
+    export LS_HOME="/usr/local/bin/gls"
+
+    export HOMEBREW_NO_ANALYTICS
+    export HOMEBREW_NO_ENV_HINTS
+    export HOMEBREW_AUTOREMOVE
+
+else
+    # Windows specific settings
+
+fi
+
+export DOTFILES_HOME="$HOME/.dotfiles"
+
+export TYPEWRITTEN_PROMPT_LAYOUT="singleline_split"
+export TYPEWRITTEN_RELATIVE_PATH="adaptive"
+export TYPEWRITTEN_BRANCH_LEFT_SYMBOL="[ "
+export TYPEWRITTEN_BRANCH_RIGHT_SYMBOL=" ]"
+export TYPEWRITTEN_SQUASH_GIT_DIRECTORIES="0"
+export TYPEWRITTEN_COLORS="git_branch:light;current_directory:green;symbol:light"
+export TYPEWRITTEN_SYMBOL="$"
+
+export ZDOTDIR="$HOME/.zsh"
+export ZSHRC_HOME="$ZDOTDIR/.zshrc"
+export EDITOR="/usr/local/bin/nvim"
+export LESSHISTFILE="/dev/null"
+export PYTHONSTARTUP="$HOME/.config/python/startup.py"
+
+PATH="$PATH:/usr/local/bin:/usr/local/sbin"
+typeset -U path
+export PATH
