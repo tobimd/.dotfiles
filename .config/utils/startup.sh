7a63d23b51f29f522622cd334781af995283a523
tobimd <tobimd@users.noreply.github.com>
2022-09-09 23:41:24 -0400

    update

--- .config/utils/startup.sh
+++ .config/utils/startup.sh
@@ -9,6 +9,7 @@ elif [[ $OSTYPE == "darwin"* ]]; then
     PATH="$PATH:/usr/local/Homebrew/bin"
 
     export LS_HOME="/usr/local/bin/gls"
+    export SED_HOME="/usr/local/opt/gnu-sed/libexec/gnubin/sed"
 
     export HOMEBREW_NO_ANALYTICS=1
     export HOMEBREW_NO_ENV_HINTS=1
@@ -19,8 +20,28 @@ else
 
 fi
 
-export DOTFILES_HOME="$HOME/.dotfiles"
+# Settings for zsh-autocomplete
+# (marlonrichert/zsh-autocomplete.git)
 
+# [no]: tab inserts the top completion.
+#  yes: tab first inserts a substring common to all listed completions, if any
+zstyle ':autocomplete:*' insert-unambiguous yes
+
+# [complete-word]: (Shift-)Tab inserts the top (bottom) completion.
+#   menu-complete: Press again to cycle to next (previous) completion.
+#     menu-select: Same as `menu-complete`, but updates selection in menu.
+zstyle ':autocomplete:*' widget-style menu-complete
+
+
+# Misc/general path or setting env variables
+export DOTFILES_HOME="$HOME/.dotfiles"
+export ZDOTDIR="$HOME/.zsh"
+export ZSHRC_HOME="$ZDOTDIR/.zshrc"
+export EDITOR="/usr/local/bin/nvim"
+export LESSHISTFILE="/dev/null"
+export PYTHONSTARTUP="$HOME/.config/python/startup.py"
+
+# Typewritten specific env variables
 export TYPEWRITTEN_PROMPT_LAYOUT="singleline_split"
 export TYPEWRITTEN_RELATIVE_PATH="adaptive"
 export TYPEWRITTEN_BRANCH_LEFT_SYMBOL="[ "
@@ -29,13 +50,6 @@ export TYPEWRITTEN_SQUASH_GIT_DIRECTORIES="0"
 export TYPEWRITTEN_COLORS="git_branch:light;current_directory:green;symbol:light"
 export TYPEWRITTEN_SYMBOL="$"
 
-export ZDOTDIR="$HOME/.zsh"
-export ZSHRC_HOME="$ZDOTDIR/.zshrc"
-export EDITOR="/usr/local/bin/nvim"
-export LESSHISTFILE="/dev/null"
-export PYTHONSTARTUP="$HOME/.config/python/startup.py"
-export DOCKER_HOST="unix:///$HOME/.local/share/containers/podman/machine/default/podman.sock"
-
-PATH="$PATH:/usr/local/bin:/usr/local/sbin"
+PATH="$PATH:/usr/local/bin:/usr/local/sbin:$HOME/.cargo/bin"
 typeset -U path
 export PATH
