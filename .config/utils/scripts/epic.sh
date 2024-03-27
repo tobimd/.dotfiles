882deba14ed1aea2db176978282720cd62883ff8
tobimd <tobimd@users.noreply.github.com>
2024-03-26 23:50:34 -0300

    feat: use scuffed script

--- .config/utils/scripts/epic.sh
+++ .config/utils/scripts/epic.sh
@@ -3,7 +3,18 @@
 epic() {
 	if [[ "$1" == "games" ]]; then
 		shift
-		legendary list 2>/dev/null | ~/.config/legendary/games.py $@
+		~/.config/legendary/games.py $@ "$(legendary list 2>/dev/null)"
+	elif [[ "$1" == "installed" ]]; then
+		games="$(~/.config/legendary/games.py --ini "$(legendary list 2>/dev/null)")"
+		installed=( $(legendary list-installed | grep -Eoe ':[^\|]+\| V' | grep -Eoe '\w\w+' --color=never) )
+		grepped=""
+
+		for game in ${installed[@]}; do
+			lines=( $(echo "$games" | grep "$game") )
+			grepped+="|${lines[1]}"
+		done
+
+		~/.config/legendary/games.py "$(legendary list 2>/dev/null)" | grep -Ee "${grepped:1}" --color=never
 	else
 		legendary $@
 	fi
